80e26a47fb67298a2f71bf4a6b5d48ccf03410e1
hawang <654296217@qq.com>
2015-07-09 13:39:33 +0800

    add sampleController

--- core/src/main/java/com/tw/SampleController.java
+++ core/src/main/java/com/tw/SampleController.java
@@ -1,7 +1,18 @@
 package com.tw;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by hgwang on 7/9/15.
  */
+@Controller
 public class SampleController {
+
+    @RequestMapping("home")
+    public ModelAndView loadHomePage() {
+
+        return new ModelAndView("home");
+    }
 }
