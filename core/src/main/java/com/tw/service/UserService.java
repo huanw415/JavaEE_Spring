f293e505ff8af4c5d6693ef94590a81bbc8012e2
hawang <654296217@qq.com>
2015-07-12 22:07:07 +0800

    add canLogIn method

--- core/src/main/java/com/tw/service/UserService.java
+++ core/src/main/java/com/tw/service/UserService.java
@@ -34,4 +34,17 @@ public class UserService {
     public void updateUser(User user){
         userDao.updateUser(user);
     }
+
+    public List<User> getUsersByName(String name){
+        return userDao.getUsersByName(name);
+    }
+
+    public String canLogIn(User user, String password){
+
+        if(user.getPassword() == password){
+            return "密码正确";
+        }else {
+            return "密码错误";
+        }
+    }
 }
