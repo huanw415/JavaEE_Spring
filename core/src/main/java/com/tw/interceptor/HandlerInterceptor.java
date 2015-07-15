0b1bfa72077ed7e78b8b95b00c39a583d1e0e07f
hawang <654296217@qq.com>
2015-07-15 13:47:13 +0800

    implement HandlerInterceptor class

--- core/src/main/java/com/tw/interceptor/HandlerInterceptor.java
+++ core/src/main/java/com/tw/interceptor/HandlerInterceptor.java
@@ -1,7 +1,41 @@
 package com.tw.interceptor;
 
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by hgwang on 7/15/15.
  */
-public class HandlerInterceptor {
+public class HandlerInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String currentUser = "";
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie: cookies){
+            if(cookie.getName().equals("current_user")){
+                currentUser = cookie.getValue();
+            }
+        }
+
+        if(currentUser.equals("")){
+            Cookie pageCookie = new Cookie("previous_page", null);
+            pageCookie.setMaxAge(0);
+            pageCookie.setPath("/");
+            response.addCookie(pageCookie);
+
+            Cookie cookie = new Cookie("previous_page", request.getRequestURI().substring(4));
+            cookie.setPath("/");
+            response.addCookie(cookie);
+
+            response.sendRedirect("/web/login");
+            return false;
+        }
+
+        return true;
+    }
 }
