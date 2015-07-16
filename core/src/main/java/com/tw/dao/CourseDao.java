38747facb84589374d46f936f847f2e25b4022da
hawang <654296217@qq.com>
2015-07-17 00:30:49 +0800

    implement courseDao and CourseService

---
+++ core/src/main/java/com/tw/dao/CourseDao.java
@@ -0,0 +1,7 @@
+package com.tw.dao;
+
+/**
+ * Created by hgwang on 7/17/15.
+ */
+public class CourseDao {
+}
---
+++ core/src/main/java/com/tw/service/CourseService.java
@@ -0,0 +1,7 @@
+package com.tw.service;
+
+/**
+ * Created by hgwang on 7/17/15.
+ */
+public class CourseService {
+}
