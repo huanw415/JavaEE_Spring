5f3aa2a6da2bcea13b8bb16183fc0dd4157f79c1
hawang <654296217@qq.com>
2015-07-16 15:49:55 +0800

    finish the inition of Employee

---
+++ core/src/main/resources/db/seed/init_employee.sql
@@ -0,0 +1,3 @@
+INSERT INTO EMPLOYEE (Name, Role, UserId) VALUES ('Ada', 'OPs', 7);
+INSERT INTO EMPLOYEE (Name, Role, UserId) VALUES ('Lily', 'coach', 6);
+INSERT INTO EMPLOYEE (Name, Role, UserId) VALUES ('王丽', 'HR', 5);
