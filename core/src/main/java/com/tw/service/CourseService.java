6999937fbd862869b5992fe2bc48d88091e40eeb
hawang <654296217@qq.com>
2015-07-17 00:32:14 +0800

    implement Courseservice class

--- core/src/main/java/com/tw/service/CourseService.java
+++ core/src/main/java/com/tw/service/CourseService.java
@@ -1,7 +1,22 @@
 package com.tw.service;
 
+import com.tw.dao.CourseDao;
+import com.tw.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by hgwang on 7/17/15.
  */
+@Service
 public class CourseService {
+
+    @Autowired
+    private CourseDao courseDao;
+
+    public List<Course> getAllCourses(){
+        return courseDao.getAllCourses();
+    }
 }
