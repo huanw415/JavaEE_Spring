ff4ea0a1f7c8cf61b190e2e9bb4a8d6fd5c4c96e
hawang <654296217@qq.com>
2015-07-16 23:35:32 +0800

    implement customer class

--- core/src/main/java/com/tw/dao/Customer.java
+++ core/src/main/java/com/tw/dao/Customer.java
@@ -4,4 +4,22 @@ package com.tw.dao;
  * Created by hgwang on 7/16/15.
  */
 public class Customer {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
