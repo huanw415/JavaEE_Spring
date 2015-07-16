766a51140325772bde44f16873c8266fbf1c6fa2
hawang <654296217@qq.com>
2015-07-16 11:30:49 +0800

    implement the Coach entity

---
+++ core/src/main/java/com/tw/entity/Coach.java
@@ -0,0 +1,44 @@
+package com.tw.entity;
+
+/**
+ * Created by hgwang on 7/16/15.
+ */
+public class Coach {
+
+    private int id;
+    private String name;
+    private String role;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
