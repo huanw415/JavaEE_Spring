6c6a595d263bb81822a8a565a27134510ae232de
hawang <654296217@qq.com>
2015-07-16 13:22:40 +0800

    add password to init_user.sql

--- core/src/main/resources/db/seed/init_user.sql
+++ core/src/main/resources/db/seed/init_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO USER (Name, Gender, Email, Age) VALUES ('赵明', '男', 'zhaoming@outlook.com', 20);
-INSERT INTO USER (Name, Gender, Email, Age) VALUES ('小黄', '女', 'xiaohuang@outlook.com', 18);
-INSERT INTO USER (Name, Gender, Email, Age) VALUES ('王丽', '女', 'wangli@outlook.com', 20);
+INSERT INTO USER (Name, Gender, Email, Age, Password) VALUES ('赵明', '男', 'zhaoming@outlook.com', 20, 'C4CA4238A0B923820DCC509A6F75849B');
+INSERT INTO USER (Name, Gender, Email, Age, Password) VALUES ('小黄', '女', 'xiaohuang@outlook.com', 18, 'C4CA4238A0B923820DCC509A6F75849B');
+INSERT INTO USER (Name, Gender, Email, Age, Password) VALUES ('王丽', '女', 'wangli@outlook.com', 20, 'C4CA4238A0B923820DCC509A6F75849B');
