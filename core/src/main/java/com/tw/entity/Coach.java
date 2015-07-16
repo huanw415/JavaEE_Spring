9b3e72b55e27864bb607b456e26acbd2f4cdf7a4
hawang <654296217@qq.com>
2015-07-16 11:40:37 +0800

    use annotation of hibernate

--- core/src/main/java/com/tw/entity/Coach.java
+++ core/src/main/java/com/tw/entity/Coach.java
@@ -1,13 +1,29 @@
 package com.tw.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by hgwang on 7/16/15.
  */
+
+@Entity
+@Table(name="employee")
 public class Coach {
 
+    @Id
+    @Column(name="Id")
     private int id;
+
+    @Column(name="Name")
     private String name;
+
+    @Column(name="Role")
     private String role;
+
+    @Column(name="UserId")
     private int userId;
 
     public int getId() {
