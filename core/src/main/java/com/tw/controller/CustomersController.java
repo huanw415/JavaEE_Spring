0a0caf699a4e6d2afe0c9c5feeea5f1460e324e9
hawang <654296217@qq.com>
2015-07-17 14:54:16 +0800

    implement getCustomersPage in CustomersController

--- core/src/main/java/com/tw/controller/CustomersController.java
+++ core/src/main/java/com/tw/controller/CustomersController.java
@@ -1,7 +1,19 @@
 package com.tw.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by hgwang on 7/17/15.
  */
+@RestController
+@RequestMapping("/")
 public class CustomersController {
+
+    @RequestMapping(value = "customers", method = RequestMethod.GET)
+    public ModelAndView getCustomersPage(){
+        return new ModelAndView("customers");
+    }
 }
