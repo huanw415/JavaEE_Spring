d4a54554c7d09cf1740d2459d89a04fe5c5111e4
hawang <654296217@qq.com>
2015-07-17 09:14:28 +0800

    finish the inition of course

---
+++ core/src/main/resources/db/seed/init_course.sql
@@ -0,0 +1,4 @@
+INSERT INTO COURSE (Name, CoachId) VALUES ('private', 2);
+INSERT INTO COURSE (Name, CoachId) VALUES ('public', 2);
+INSERT INTO COURSE (Name, CoachId) VALUES ('public', 4);
+INSERT INTO COURSE (Name, CoachId) VALUES ('private', 4);
