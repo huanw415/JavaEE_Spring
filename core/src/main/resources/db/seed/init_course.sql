91bef6577397053646424d77579e451d75406a68
hawang <654296217@qq.com>
2015-07-17 16:27:35 +0800

    modify the inition of course

--- core/src/main/resources/db/seed/init_course.sql
+++ core/src/main/resources/db/seed/init_course.sql
@@ -1,4 +1,4 @@
 INSERT INTO COURSE (Name, CoachId) VALUES ('private', 2);
-INSERT INTO COURSE (Name, CoachId) VALUES ('public', 2);
-INSERT INTO COURSE (Name, CoachId) VALUES ('public', 4);
+INSERT INTO COURSE (Name, CoachId) VALUES ('swimming', 2);
+INSERT INTO COURSE (Name, CoachId) VALUES ('running', 4);
 INSERT INTO COURSE (Name, CoachId) VALUES ('private', 4);
