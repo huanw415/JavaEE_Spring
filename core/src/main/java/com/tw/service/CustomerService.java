6ef027d9c693b0a9ded78d77a9544c3a7eec1bd3
hawang <654296217@qq.com>
2015-07-17 13:55:05 +0800

    add getAllCustomers in CustomerService class

---
+++ core/src/main/java/com/tw/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.tw.service;
+
+import com.tw.dao.CustomerDao;
+import com.tw.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by hgwang on 7/17/15.
+ */
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerDao customerDao;
+
+    public List<Customer> getAllCustomers(){
+        return customerDao.getAllCustomers();
+    }
+}
